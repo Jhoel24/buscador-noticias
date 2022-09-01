ab3ca84aca309d19c9806e543826a7b630ef2292
Jhoel Moreno <jhoel.moreno24@gmail.com>
2022-09-01 13:01:21 -0600

    finish

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { Container, Grid, Typography } from '@mui/material';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
+import { NoticiasProvider } from './context/NoticiasProvider';
+
+function App() {
+   return (
+      <NoticiasProvider>
+        <Container>
+          <header>
+            <Typography 
+              align='center' 
+              sx={{
+              fontWeight: 'bold',
+              textTransform: 'uppercase',
+              fontSize: '2rem',
+              letterSpacing: '2px'
+              }} 
+              marginY={5}
+              component='h1'
+              variant='h3'
+            >
+              Buscador de noticias
+            </Typography>
+          </header>
+
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item md={6} xs={12} lg={4} >
+              <Formulario />  
+            </Grid>
+          </Grid>
+          <ListadoNoticias />
+        </Container>
+      </NoticiasProvider>
+  )
+}
+
+export default App
